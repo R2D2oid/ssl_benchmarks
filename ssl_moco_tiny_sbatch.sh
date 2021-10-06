2c05b74a5eeb3aa17e5e49f7d60a4f86f3701e96
Zahra Vaseqi <zahrav@beluga3.int.ets1.calculquebec.ca>
2021-10-06 19:09:15 -0400

    added tiny ssl training scripts

---
+++ ssl_moco_tiny_sbatch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=def-mudl
+#SBATCH --gres=gpu:v100:1              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=8              # CPU cores/threads
+#SBATCH --mem=50G                      # memory per node
+#SBATCH --time=0-10:00                  # time (DD-HH:MM)
+#SBATCH --output=ssl_moco_tiny.out
+
+source env_ssl/bin/activate
+
+#python3 tinyimage_benchmarks.py --user_name='srangrej' --model_name='SimCLR'
+#python3 tinyimage_benchmarks.py --user_name='srangrej' --model_name='SwAV'
+python3 tinyimage_benchmarks.py --user_name='zahrav' --model_name='MoCo' --data '/home/zahrav/projects/def-jjclark/shared_data/tinyimagenet/data/tiny-imagenet-200'
+#python3 tinyimage_benchmarks.py --user_name='zahrav' --model_name='MoCo' --data '~/projects/def-jjclark/shared_data/tinyimagenet/data/tiny-imagenet-200'
+#python3 tinyimage_benchmarks.py --user_name='iamara' --model_name='SimSiam'
+#python3 tinyimage_benchmarks.py --user_name='iamara' --model_name='BYOL'
